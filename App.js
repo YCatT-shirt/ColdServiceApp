3b3fa85d22c2018da9bcacec549a1317cdbc459b
YCatT-shirt <silva.cruzomarabraham@gmail.com>
2025-05-08 21:15:31 -0700

    Fix: Corrige errores de navegación y módulos faltantes

--- App.js
+++ App.js
@@ -1,20 +1,141 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//Bibliotecas necesarias.
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AuthProvider } from './context/AuthContext'; // Importa el AuthProvider
+import { ThemeProvider } from './context/ThemeContext';
+import HomeTabs from './navigation/HomeTabs';
+import 'react-native-gesture-handler';
 
-export default function App() {
+// Importamos todas las pantallas.
+import LoginScreen from './navigation/LoginScreen';
+import UserList from './user/UserList';
+import CreateUserScreen from './user/CreateUserScreen';
+import UserDetailScreen from './user/UserDetailScreen';
+import ReportesScreen from './reports/ReportesScreen';
+import ReporteViaticosScreen from './reports/ReporteViaticosScreen';
+import ConsultarReportesScreen from './querys/ConsultarReportesScreen';
+import ConfigScreen from './utils/ConfigScreen';
+import ProcesoReparacionScreen from './reports/ProcesoReparacionScreen';
+import ConsultarProcesoReparacionesScreen from './querys/ConsultarProcesoReparacionesScreen';
+import ReportarErrorScreen from './utils/ReportarErrorScreen';
+import ConsultarErroresScreen from './querys/ConsultarErrores';
+import CostosReparacionesScreen from './reports/CostoReparacionesScreen';
+import Chat from './utils/Chat';
+import ChatRoom from './utils/ChatRoom';
+//_______________________________________________________________________________________________________________________________
+
+// Crea el navegador de pila
+const Stack = createNativeStackNavigator();
+
+// Define la pila de navegación
+function MyStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+      <Stack.Navigator>
+        {/* Pantalla de inicio de sesión */}
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{ headerShown: false }} // Oculta el header en LoginScreen
+        />
+        {/* Pantalla principal */}
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeTabs} // ahora usa el Bottom Tabs
+          options={{ headerShown: false }}
+        />
+        {/* Otras pantallas */}
+        <Stack.Screen
+          name="UserList"
+          component={UserList}
+          options={{ title: 'Lista de Usuarios' }}
+        />
+        <Stack.Screen
+          name="CostoReparacionesScreen"
+          component={CostosReparacionesScreen}
+          options={{ title: 'Costo de reparaciones' }}
+        />
+        <Stack.Screen
+          name="CreateUserScreen"
+          component={CreateUserScreen}
+          options={{ title: 'Crear Usuario' }}
+        />
+        <Stack.Screen
+          name="UserDetailScreen"
+          component={UserDetailScreen}
+          options={{ title: 'Detalle del Usuario' }}
+        />
+        <Stack.Screen
+          name="ReportesScreen"
+          component={ReportesScreen}
+          options={{ title: 'Reportes de Servicio' }}
+        />
+        <Stack.Screen
+          name="ConsultarReportesScreen"
+          component={ConsultarReportesScreen}
+          options={{ title: 'Consultar Reportes' }}
+        />
+        {/* Nueva pantalla de reporte de viáticos */}
+        <Stack.Screen
+          name="ReporteViaticosScreen" // Registra la pantalla de reporte de viáticos
+          component={ReporteViaticosScreen}
+          options={{ title: 'Reporte de Viáticos' }}
+        />
+        {/* Pantalla de configuración */}
+        <Stack.Screen
+          name="ConfigScreen"
+          component={ConfigScreen}
+          options={{ title: 'Configuración' }}
+        />
+        {/* Pantalla de PROCESO DE REPARACION */}
+        <Stack.Screen
+          name="ProcesoReparacionScreen"
+          component={ProcesoReparacionScreen}
+          options={{ title: 'Proceso de Reparación' }}
+        />
+        {/* Pantalla de CONSULTAR PROCESO DE REPARACION */}
+        <Stack.Screen
+          name="ConsultarProcesoReparacionesScreen"
+          component={ConsultarProcesoReparacionesScreen}
+          options={{ title: 'Consultar Reportes de Reparación' }}
+        />
+
+        {/* Pantalla de Reportar Error */}
+        <Stack.Screen
+          name="ReportarErrorScreen"
+          component={ReportarErrorScreen}
+          options={{ title: 'Reportar Error' }}
+        />
+        {/* Pantalla de CONULTAR Error */}
+        <Stack.Screen
+          name="ConsultarErroresScreen"
+          component={ConsultarErroresScreen}
+          options={{ title: 'Consultar Errores' }}
+        />
+        {/* Pantalla de CHAT */}
+        <Stack.Screen
+          name="Chat"
+          component={Chat}
+          options={{ title: 'Chat' }}
+        />
+        <Stack.Screen
+          name="ChatRoom"
+          component={ChatRoom}
+          options={{ title: 'Sala de Chat' }}
+        />
+      </Stack.Navigator>
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// Componente principal de la aplicación
+export default function App() {
+  return (
+    <AuthProvider>
+      <NavigationContainer>
+        <MyStack />
+      </NavigationContainer>
+    </AuthProvider>
+  );
+}
